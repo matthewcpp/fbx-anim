7e63b864895338ff37c697d2f0531300b615cd87
Chris <c.a.dryden@gmail.com>
2012-09-26 20:02:56 -0700

    fixing path and assets so things will actually compile

--- fbx-load/fbx-load/main.cpp
+++ fbx-load/fbx-load/main.cpp
@@ -5,7 +5,7 @@
 
 int main (int argc, char** argv){
 
-	std::string path = "C:/development/content/dude/dude.fbx";
+	std::string path = "C:/development/fbx-anim/assets/dude/dude.fbx";
 
 	FbxManager* fbxManager = FbxManager::Create();
 	FbxImporter* fbxImporter = FbxImporter::Create(fbxManager, "fbx importer");
